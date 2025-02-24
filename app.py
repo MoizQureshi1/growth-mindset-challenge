afa6fdb324acc90d726a3bd90a7d4955e01a6ad2
Moiz Qureshi <159722436+MoizQureshi1@users.noreply.github.com>
2025-02-24 19:23:19 +0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config ( page_title = "Growth Mindset Project", project_icon = "🟊" )
+st.set_page_config ( page_title = "Growth Mindset Project", project_icon = "🌟" )
 st.title ("Growth Mindset Challenge: Web App with Streamlit")
 
 st.header ("🚀 Welcome to your growyh journey!")
